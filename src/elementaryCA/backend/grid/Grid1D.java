32651a515957d98d5b8c51c73e6ea92f8c2141d0
hieu9 <hquang@unm.edu>
2020-11-18 19:48:03 -0700

    Commit

--- src/elementaryCA/backend/grid/Grid1D.java
+++ src/elementaryCA/backend/grid/Grid1D.java
@@ -11,6 +11,16 @@ import static elementaryCA.backend.cell.Cell.ONE;
 import static elementaryCA.backend.cell.Cell.ZERO;
 
 public class Grid1D {
+    // Conditions
+    private char s111;
+    private char s110;
+    private char s101;
+    private char s100;
+    private char s011;
+    private char s010;
+    private char s001;
+    private char s000;
+
     // Used by JavaFX to display the visualization
     private final GridPane gridPane;
     // 8 bit string representing the behavior of the CA
@@ -20,6 +30,7 @@ public class Grid1D {
 
     // Current generation of cells
     private List<Cell> currentGen;
+
     // Current generation (row) being shown to the screen
     private int currentGenIndex;
 
@@ -36,6 +47,17 @@ public class Grid1D {
         show();
     }
 
+    private void setBehaviours () {
+        s111 = behavior.charAt(0);
+        s110 = behavior.charAt(1);
+        s101 = behavior.charAt(2);
+        s100 = behavior.charAt(3);
+        s011 = behavior.charAt(4);
+        s010 = behavior.charAt(5);
+        s001 = behavior.charAt(6);
+        s000 = behavior.charAt(7);
+    }
+
     /**
      * TODO: Fill in the logic below
      * I would suggest starting off by hard coding one of the rules,
@@ -43,155 +65,69 @@ public class Grid1D {
      * This function evolves the current generation to the next generation
      * using the current rule set given by the behavior string.
      */
+
+    private void cellTransform(int right, int left, int middle, List<Cell> nextGen) {
+        if (currentGen.get(middle) == ONE) {
+            // Condition 111
+            if (currentGen.get(right) == ONE // RIGHT
+                    && currentGen.get(left) == ONE) { // LEFT
+                nextGen.add(Cell.fromChar(s111));
+            }
+            // Condition 011
+            else if (currentGen.get(right) == ONE
+                    && currentGen.get(left) == ZERO) {
+                nextGen.add(Cell.fromChar(s011));
+            }
+            // Condition 110
+            else if (currentGen.get(right) == ZERO
+                    && currentGen.get(left) == ONE) {
+                nextGen.add(Cell.fromChar(s110));
+            }
+            // Condition 010
+            else if (currentGen.get(right) == ZERO
+                    && currentGen.get(left) == ZERO) {
+                nextGen.add(Cell.fromChar(s010));
+            }
+        }
+        // Middle Number 0
+        else if (currentGen.get(middle) == ZERO) {
+            // Condition 101
+            if (currentGen.get(right) == ONE
+                    && currentGen.get(left) == ONE) {
+                nextGen.add(Cell.fromChar(s101));
+            }
+            // Condition 001
+            else if (currentGen.get(right) == ONE
+                    && currentGen.get(left) == ZERO) {
+                nextGen.add(Cell.fromChar(s001));
+            }
+            // Condition 100
+            else if (currentGen.get(right) == ZERO
+                    && currentGen.get(left) == ONE) {
+                nextGen.add(Cell.fromChar(s100));
+            }
+            // Condition 000
+            else if (currentGen.get(right) == ZERO
+                    && currentGen.get(left) == ZERO) {
+                nextGen.add(Cell.fromChar(s000));
+            }
+        }
+    }
+
     private void evolve() {
+
         List<Cell> nextGen = new ArrayList<>();
         for (int i = 0; i < currentGen.size(); i++) {
             if (i == 0) {
-                // Middle Number 1
-                if (currentGen.get(i) == ONE) {
-                    // Condition 111
-                    if (currentGen.get(i + 1) == ONE // RIGHT
-                            && currentGen.get(currentGen.size() - 1) == ONE) { // LEFT
-                        nextGen.add(Cell.fromChar('0'));
-                    }
-                    // Condition 011
-                    else if (currentGen.get(i + 1) == ONE
-                            && currentGen.get(currentGen.size() - 1) == ZERO) {
-                        nextGen.add(Cell.fromChar('1'));
-                    }
-                    // Condition 110
-                    else if (currentGen.get(i + 1) == ZERO
-                            && currentGen.get(currentGen.size() - 1) == ONE) {
-                        nextGen.add(Cell.fromChar('0'));
-                    }
-                    // Condition 010
-                    else if (currentGen.get(i + 1) == ZERO
-                            && currentGen.get(currentGen.size() - 1) == ZERO) {
-                        nextGen.add(Cell.fromChar('1'));
-                    }
-                }
-                // Middle Number 0
-                else if (currentGen.get(i) == ZERO) {
-                    // Condition 101
-                    if (currentGen.get(i + 1) == ONE
-                            && currentGen.get(currentGen.size() - 1) == ONE) {
-                        nextGen.add(Cell.fromChar('0'));
-                    }
-                    // Condition 001
-                    else if (currentGen.get(i + 1) == ONE
-                            && currentGen.get(currentGen.size() - 1) == ZERO) {
-                        nextGen.add(Cell.fromChar('1'));
-                    }
-                    // Condition 100
-                    else if (currentGen.get(i + 1) == ZERO
-                            && currentGen.get(currentGen.size() - 1) == ONE) {
-                        nextGen.add(Cell.fromChar('1'));
-                    }
-                    // Condition 000
-                    else if (currentGen.get(i + 1) == ZERO
-                            && currentGen.get(currentGen.size() - 1) == ZERO) {
-                        nextGen.add(Cell.fromChar('0'));
-                    }
-                }
+                cellTransform(i + 1,currentGen.size() - 1, i, nextGen);
             }
             else if (i == currentGen.size() - 1) {
-                // Middle Number 1
-                if (currentGen.get(i) == ONE) {
-                    // Condition 111
-                    if (currentGen.get(0) == ONE
-                            && currentGen.get(i - 1) == ONE) {
-                        nextGen.add(Cell.fromChar('0'));
-                    }
-                    // Condition 011
-                    else if (currentGen.get(i) == ONE
-                            && currentGen.get(i - 1) == ZERO) {
-                        nextGen.add(Cell.fromChar('1'));
-                    }
-                    // Condition 110
-                    else if (currentGen.get(0) == ZERO
-                            && currentGen.get(i - 1) == ONE) {
-                        nextGen.add(Cell.fromChar('0'));
-                    }
-                    // Condition 010
-                    else if (currentGen.get(0) == ZERO
-                            && currentGen.get(i - 1) == ZERO) {
-                        nextGen.add(Cell.fromChar('1'));
-                    }
-                }
-                // Middle Number 0
-                else if (currentGen.get(i) == ZERO) {
-                    // Condition 101
-                    if (currentGen.get(0) == ONE
-                            && currentGen.get(i - 1) == ONE) {
-                        nextGen.add(Cell.fromChar('0'));
-                    }
-                    // Condition 001
-                    else if (currentGen.get(0) == ONE
-                            && currentGen.get(i - 1) == ZERO) {
-                        nextGen.add(Cell.fromChar('1'));
-                    }
-                    // Condition 100
-                    else if (currentGen.get(0) == ZERO
-                            && currentGen.get(i - 1) == ONE) {
-                        nextGen.add(Cell.fromChar('1'));
-                    }
-                    // Condition 000
-                    else if (currentGen.get(0) == ZERO
-                            && currentGen.get(i - 1) == ZERO) {
-                        nextGen.add(Cell.fromChar('0'));
-                    }
-                }
+                cellTransform(0, i - 1, i, nextGen);
             }
             else {
-                // Middle Number 1
-                if (currentGen.get(i) == ONE) {
-                    // Condition 111
-                    if (currentGen.get(i + 1) == ONE // RIGHT
-                            && currentGen.get(i - 1) == ONE) { // LEFT
-                        nextGen.add(Cell.fromChar('0'));
-                    }
-                    // Condition 011
-                    else if (currentGen.get(i + 1) == ONE
-                            && currentGen.get(i - 1) == ZERO) {
-                        nextGen.add(Cell.fromChar('1'));
-                    }
-                    // Condition 110
-                    else if (currentGen.get(i + 1) == ZERO
-                            && currentGen.get(i - 1) == ONE) {
-                        nextGen.add(Cell.fromChar('0'));
-                    }
-                    // Condition 010
-                    else if (currentGen.get(i + 1) == ZERO
-                            && currentGen.get(i - 1) == ZERO) {
-                        nextGen.add(Cell.fromChar('1'));
-                    }
-                }
-                // Middle Number 0
-                else if (currentGen.get(i) == ZERO) {
-                    // Condition 101
-                    if (currentGen.get(i + 1) == ONE
-                            && currentGen.get(i - 1) == ONE) {
-                        nextGen.add(Cell.fromChar('0'));
-                    }
-                    // Condition 001
-                    else if (currentGen.get(i + 1) == ONE
-                            && currentGen.get(i - 1) == ZERO) {
-                        nextGen.add(Cell.fromChar('1'));
-                    }
-                    // Condition 100
-                    else if (currentGen.get(i + 1) == ZERO
-                            && currentGen.get(i - 1) == ONE) {
-                        nextGen.add(Cell.fromChar('1'));
-                    }
-                    // Condition 000
-                    else if (currentGen.get(i + 1) == ZERO
-                            && currentGen.get(i - 1) == ZERO) {
-                        nextGen.add(Cell.fromChar('0'));
-                    }
-                }
+                cellTransform(i + 1,i - 1, i, nextGen);
             }
         }
-
         // Inserting newGen to currentGen
         currentGen = nextGen;
     }
@@ -219,6 +155,7 @@ public class Grid1D {
      * It then shows this new generation to the Java FX window.
      */
     public void nextGeneration() {
+        setBehaviours();
         evolve();
         show();
     }
