8c6718114552304018e774a347878618b7847629
hieu9 <hquang@unm.edu>
2020-11-20 18:01:59 -0700

    Commit

---
+++ src/gameOfLife/backend/Main.java
@@ -0,0 +1,21 @@
+package gameOfLife.backend;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+/**
+ * Author: Hieu Quang
+ * Version: 2.0
+ * Date: 20/11/20
+ * This class is the main of the program.
+ */
+public class Main extends Application {
+    public static void main(String[] args) throws IOException {
+        launch(args);
+    }
+
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
