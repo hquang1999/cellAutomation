e8cb9eb1c69c6fe793c68cfa76c8ff331ac1c302
hieu9 <hquang@unm.edu>
2020-11-20 18:05:07 -0700

    Commit

---
+++ src/gameOfLife/backend/Runner.java
@@ -0,0 +1,29 @@
+package gameOfLife.backend;
+
+//import gameOfLife.backend.grid.Grid1D;
+import javafx.animation.AnimationTimer;
+
+import java.util.concurrent.TimeUnit;
+
+public class Runner {
+
+    /**
+     * Starts the animation timer that controls the visualization.
+     * @param grid of cells to be shown to the screen. At each time step
+     *             it is evolved using its given ruleset.
+     */
+    /*
+    public static void run(Grid1D grid) {
+        AnimationTimer timer = new AnimationTimer() {
+            private long prevUpdate = 0;
+            @Override
+            public void handle(long now) {
+                grid.nextGeneration();
+                prevUpdate = now;
+            }
+        };
+        timer.start();
+    }
+
+     */
+}
